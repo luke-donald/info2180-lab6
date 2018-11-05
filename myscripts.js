1db030d3d82bd1763db77ec76cec1e55b6bc402e
Luke Donald <luke.donald22@gmail.com>
2018-11-05 05:16:21 +0000

    Completed Exercise 4

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,14 @@
+//Luke Donald
+//INFO2180 Lab 6
+//ID No. 620103232
 
 window.onload = function() {
     
     var searchButton = document.getElementById("button");
+    var searchAllButton = document.getElementById("getallbutton");
     
     searchButton.onclick = printDefn;
+    searchAllButton.onclick = printAllDefn;
     
 };
 
@@ -19,12 +24,25 @@ function printDefn(){
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
             
             if (httpRequest.status === 200) {
-                var response = httpRequest.responseText;
-                document.getElementById("test").innerHTML = "hello testing output";
-                document.getElementById("result").innerHTML = response;
-                //alert(response);
+                
+                var response = httpRequest.responseXML;
+                
+                for(var i = 0; i < response.getElementsByTagName("definition").length; i++){
+                    
+                    if(response.getElementsByTagName("definition")[i].getAttribute("name") === searchWord){
+                        
+                        var tempIndex = i;
+                    }
+                }
+                
+                var tempName = "<h3>" + response.getElementsByTagName("definition")[tempIndex].getAttribute("name").toUpperCase() + "</h3>";
+                var tempAuthor = "<p>- " + response.getElementsByTagName("definition")[tempIndex].getAttribute("author") + "</p>";
+                var tempDefn = "<p>" + response.getElementsByTagName("definition")[tempIndex].innerHTML + "</p>";
+                
+                document.getElementById("result").innerHTML = tempName + tempDefn + tempAuthor;
                 
             } else {
+                
                 alert('There was a problem with the request.');
             }
         }
@@ -33,5 +51,43 @@ function printDefn(){
     
     httpRequest.open('GET', url, true);
     httpRequest.send();
+    
+}
+
+function printAllDefn(){
+    
+    var httpRequest = new XMLHttpRequest();
+    var url = "https://info2180-lab6-lukedonald.c9users.io/request.php?q=&all=true";
+    
+    httpRequest.onreadystatechange = function(){
+        
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            
+            if (httpRequest.status === 200) {
+                
+                var response = httpRequest.responseXML;
+                
+                var ans = "";
+                
+                for(var i = 0; i < response.getElementsByTagName("definition").length; i++){
+                    
+                    ans += "<li><h3>" + response.getElementsByTagName("definition")[i].getAttribute("name").toUpperCase() + "</h3></li>";
+                    ans += "<p>" + response.getElementsByTagName("definition")[i].innerHTML + "</p>";
+                    ans += "<p>- " + response.getElementsByTagName("definition")[i].getAttribute("author") + "</p>";
+                }
+                
+                document.getElementById("result").innerHTML = "<ol>" + ans + "</ol>";
+                
+            } else {
+                
+                alert('There was a problem with the request.');
+            }
+        }
+        
+    };
+    
+    httpRequest.open('GET', url, true);
+    httpRequest.send();
+    
     
 }
--- request.php
+++ request.php
@@ -3,16 +3,39 @@
 // accept a term (keyword)
 // respond with a value
 
-$query = $_GET['q'];
-$definition = [
-    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic beverages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
-    "html" => "The standard markup language for creating web pages and web applications.",
-    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
-    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+<entries>
+    <definition name = "definition" author = "Matthew">
+        A statement of the exact meaning of a word, especially in a dictionary.
+    </definition>
+    
+    <definition name = "bar" author = "Mark">
+        A place that sells alcholic beverages
+    </definition>
+
+    <definition name = "ajax" author = "Luke">
+        Technique which involves the use of javascript and xml (or JSON)
+    </definition>
+    
+    <definition name = "html" author = "John">
+        The standard markup language for creating web pages and web applications.
+    </definition>
+
+    <definition name = "css" author = "Moses">
+        A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+
+    <definition name = "javascript" author = "Mary">
+        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+
+    <definition name = "php" author = "Joseph">
+        A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+    </definition>
+
+</entries>';
+
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo $xmlOutput->asXML();
